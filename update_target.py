772f2a5b752e162d38f4ac9552eabb2027df38ca
Klaus <70058709+klau5dev@users.noreply.github.com>
2021-04-12 11:32:18 +0000

    Update update_target.py - Update private target

--- update_target.py
+++ update_target.py
@@ -1,22 +1,39 @@
 import os
 import json
 
-TARGET_PATH = os.environ['HOME'] + "/targets/chaos-bugbounty-list.json" # https://github.com/projectdiscovery/public-bugbounty-programs clone folder
+TARGET_PATH = os.environ['HOME']
+PUBLIC_TARGET_PATH = TARGET_PATH + "/targets/chaos-bugbounty-list.json" # https://github.com/projectdiscovery/public-bugbounty-programs clone
+PRIVATE_TARGET_PATH = os.environ['HOME'] + "/targets/private-list.json" # same format with https://github.com/projectdiscovery/public-bugbounty-programs
+
 WORKSPACE_PATH = os.environ['HOME'] + "/workspace/"
 
-with open(TARGET_PATH, 'r') as f:
-    targets = json.loads(f.read())['programs']
+def update_workspace(targets):
+    if not os.path.isdir(WORKSPACE_PATH):
+        os.mkdir(WORKSPACE_PATH)
 
-if not os.path.isdir(WORKSPACE_PATH):
-    os.mkdir(WORKSPACE_PATH)
+    for target in targets:
+        path = WORKSPACE_PATH + target['name']
+        if not os.path.isdir(path):
+            os.mkdir(path)
 
-for target in targets:
-    path = WORKSPACE_PATH + target['name']
-    if not os.path.isdir(path):
-        os.mkdir(path)
+        domain_manual_path = WORKSPACE_PATH + target['name'] + '/domain_manual'
+        with open(domain_manual_path, "w") as f:
+            new_domains = sorted(list(set(target['domains'])))
 
-    domain_manual_path = WORKSPACE_PATH + target['name'] + '/domain_manual'
-    with open(domain_manual_path, "w") as f:
-        new_domains = sorted(list(set(target['domains'])))
+            f.writelines("\n".join(new_domains))
 
-        f.writelines("\n".join(new_domains))
+
+if __name__ == "__main__":
+    try:
+        with open(PUBLIC_TARGET_PATH, 'r') as f:
+            targets = json.loads(f.read())['programs']
+        update_workspace(targets)
+    except:
+        print("No public target")
+    
+    try:
+        with open(PRIVATE_TARGET_PATH, 'r') as f:
+            targets = json.loads(f.read())['programs']
+        update_workspace(targets)
+    except:
+        print("No private target")
